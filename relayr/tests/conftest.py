61a7b3371e420451357581be42751da179dc225a
Dinu Gherman <gherman@darwin.in-berlin.de>
2014-10-23 17:12:17 +0200

    Removed unused code.

--- relayr/tests/conftest.py
+++ relayr/tests/conftest.py
@@ -9,15 +9,6 @@ import sys
 import pytest
 
 
-@pytest.fixture()
-def anonymous_user():
-    "A test fixture for an anonymous user."
-    env = {
-        'userEmail': 'god@in.heaven',
-        'result': '42'
-    }
-    return env
-
 @pytest.fixture(scope='module')
 def fix_anonymous():
     "Return fixture for anonymous API access."
