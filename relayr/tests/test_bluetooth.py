3c894601921455e0bbed4fc0d97d4d2b2195b258
Dinu Gherman <gherman@darwin.in-berlin.de>
2014-10-23 16:09:09 +0200

    Migrated from unittest to pytest.

--- relayr/tests/test_bluetooth.py
+++ relayr/tests/test_bluetooth.py
@@ -7,75 +7,54 @@ Tests for accessing Relayr Wunderbar devices via Bluetooth LE.
 
 import sys
 import commands
-import unittest
 
+import pytest
 try:
     import bluetooth
     HAVE_BLUETOOTH = True
 except NotImplementedError:
     HAVE_BLUETOOTH = False
 
-from relayr import ble
-
 
 HAVE_BLUEZ = commands.getoutput('which hcitool') != ''
+ON_LINUX = sys.platform.startswith("linux")
 
 
-class BluetoothTestCase(unittest.TestCase):
+class BluetoothTestCase(object):
     "Tests using PyBluez."
 
-    @unittest.skipUnless(HAVE_BLUETOOTH, "requires working bluetooth package")
-    @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
+    @pytest.mark.skipif(not HAVE_BLUETOOTH, reason="requires working bluetooth package")
+    @pytest.mark.skipif(not ON_LINUX, reason="requires Linux")
     def test_service_discovery(self):
-        """
-        Test service discovery.
-        """
-
-        # perform a simple device discovery (not LE!) followed by a remote 
-        # name request of each discovered device
-
+        "Test service (not LE!) discovery."
         try:
+            # if found contains a (addr, name) tuple per device
             nearby_devices = bluetooth.discover_devices(lookup_names=True)
         except bluetooth.btcommon.BluetoothError:
             nearby_devices = []
-        
-        if nearby_devices:
-            print("Found %d Bluetooth devices." % len(nearby_devices))
-            for addr, name in nearby_devices:
-                print("  %s - %s" % (addr, name))
+        assert len(devs) > 0
 
 
-class BleTestCase(unittest.TestCase):
+@pytest.mark.skipif(not ON_LINUX, reason="requires Linux")
+class BleTestCase(object):
     "Tests for accessing BLE devices via Bluez tools (needs root permissions)."
 
-    @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
     def test_ble_scan(self):
-        """
-        Test BLE service discovery (needs switched on BLE devices!).
-        """
+        "Test BLE service discovery (needs switched on BLE devices!)."
+        from relayr.ble import scan_ble_devices
         devs = scan_ble_devices(timeout=3)
-        self.assertTrue(len(devs) > 0
-
+        assert len(devs) > 0
 
     def test_access_device(self):
-        """
-        Test accessing BLE device.
-        """
+        "Test accessing BLE device."
+        from relayr.ble import WunderbarGattDevice
         from config import testset4 as context
         addr = context['bluetoothAddr']
-        d = ble.SimpleBleDeviceWunderbar(addr)
-
-        prim = d.primary()
-        self.assertTrue(len(prim) > 0)
-
-        chars = d.characteristics()
-        self.assertTrue(len(chars) > 0)
-
-        char_desc = d.char_desc()
-        self.assertTrue(len(char_desc) > 0)
-
-        data = d.char_read_hnd(0x0016)
-        self.assertTrue(len(data) > 0)
+        d = WunderbarGattDevice(addr)
+        assert len(d.primary()) > 0
+        assert len(d.characteristics()) > 0
+        assert len(d.char_desc()) > 0
+        assert len(d.char_read_hnd(0x0016)) > 0
 
 
 if __name__ == "__main__":
