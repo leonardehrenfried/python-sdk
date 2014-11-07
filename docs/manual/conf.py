5d372e5a0bc07799fcc104e08cb62d65838d7cea
Dinu Gherman <gherman@darwin.in-berlin.de>
2014-11-07 17:23:06 +0100

    Minor doc changes/additions.

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Relayr Python API Client documentation build configuration file, created by
+# Relayr Python Client documentation build configuration file, created by
 # sphinx-quickstart on Mon Sep 22 15:51:28 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -16,14 +16,13 @@ import sys
 import os
 from os.path import abspath
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from
+# docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 
 
 # Add location of our Python package to be documented to the sys.path:
@@ -65,7 +64,7 @@ source_suffix = '.txt'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Relayr Python API Client'
+project = u'Relayr Python Client'
 copyright = u'2014, relayr.io'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -200,7 +199,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'RelayrPythonAPIClientdoc'
+htmlhelp_basename = 'RelayrPythonClientdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -220,7 +219,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'RelayrPythonAPIClient.tex', u'Relayr Python API Client Documentation',
+  ('index', 'RelayrPythonClient.tex', u'Relayr Python Client Documentation',
    u'relayr.io', 'manual'),
 ]
 
@@ -250,7 +249,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'relayrpythonapiclient', u'Relayr Python API Client Documentation',
+    ('index', 'relayrpythonclient', u'Relayr Python Client Documentation',
      [u'relayr.io'], 1)
 ]
 
@@ -264,8 +263,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'RelayrPythonAPIClient', u'Relayr Python API Client Documentation',
-   u'relayr.io', 'RelayrPythonAPIClient', 'One line description of project.',
+  ('index', 'RelayrPythonClient', u'Relayr Python Client Documentation',
+   u'relayr.io', 'RelayrPythonClient', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -285,13 +284,13 @@ texinfo_documents = [
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = u'Relayr Python API Client'
+epub_title = u'Relayr Python Client'
 epub_author = u'relayr.io'
 epub_publisher = u'relayr.io'
 epub_copyright = u'2014, relayr.io'
 
 # The basename for the epub file. It defaults to the project name.
-#epub_basename = u'Relayr Python API Client'
+#epub_basename = u'Relayr Python Client'
 
 # The HTML theme for the epub output. Since the default themes are not optimized
 # for small screen space, using the same theme for HTML and epub output is
