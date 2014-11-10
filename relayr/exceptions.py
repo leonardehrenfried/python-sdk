839b2cba9b26ce204cd80d33b16e514c980ee9c7
danauzi <dana@relayr.de>
2014-11-10 20:58:35 +0100

    update exceptions file

--- relayr/exceptions.py
+++ relayr/exceptions.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 """
-This module contains exceptions raised when certain operations on the 
-Relayr API fail, like missing credentials, invalid UIDs, etc.
+This module contains exceptions raised when an operation on the 
+relayr API or the relayr platform fails due to, 
+for example: missing credentials, invalid UIDs, etc.
 
-Right now two exception classes are provided: 
+At the moment two exception classes are provided: 
 
-- ``RelayrApiException`` is raised for exceptions caused by API calls
-- ``RelayrException`` is raised for other exceptions
+- ``RelayrApiException``: raised for exceptions caused by API calls
+- ``RelayrException``: raised for other exceptions
 
 :copyright: Copyright 2014 by the relayr.io team, see AUTHORS.
 :license: MIT, see LICENSE for details.
