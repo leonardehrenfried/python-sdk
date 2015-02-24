ffa3d9e4f59bc11eca9c11179f7cad0e0a0b9e31
Dinu Gherman <gherman@darwin.in-berlin.de>
2015-02-24 16:10:18 +0100

    Added MQTT data streams plus tests.

--- relayr/dataconnection.py
+++ relayr/dataconnection.py
@@ -6,12 +6,16 @@ Data Connection Classes
 This module provide connection classes for accessing device data.
 """
 
+import ssl
 import time
 import threading
+import platform
 
 from Pubnub import Pubnub
+import paho.mqtt.client as mqtt
 
 from relayr import config
+from relayr.compat import PY2, PY3
 
 
 class PubnubDataConnection(threading.Thread):
@@ -43,7 +47,9 @@ class PubnubDataConnection(threading.Thread):
             ssl_on=True
         )
 
-        if "daemon" in Pubnub.__init__.func_code.co_varnames:
+        if PY2 and "daemon" in Pubnub.__init__.func_code.co_varnames:
+            self.hub.daemon = True
+        elif PY3 and "daemon" in Pubnub.__init__.__code__.co_varnames:
             self.hub.daemon = True
 
         self.setDaemon(True)
@@ -85,20 +91,111 @@ class PubnubDataConnection(threading.Thread):
         self.hub.unsubscribe(channel_name)
 
 
-class MqttDataConnection(object):
-    "..."
+class MqttStream(threading.Thread):
+    "MQTT stream reading data from devices in the relayr cloud."
 
-    def __init__(self, callback, credentials):
+    def __init__(self, callback, devices, transport='mqtt'):
+        """
+        Opens an MQTT connection with a callback and one or more devices.
+
+        :param callback: A callable to be called with two arguments:
+            the topic and payload of a message.
+        :type callback: A function/method or object implementing the ``__call__`` method.
+        :param devices: Device objects from which to receive data.
+        :type devices: list
+        :param transport: Name of the transport method, right now only 'mqtt'.
+        :type transport: string
+        """
+        super(MqttStream, self).__init__()
+        self._stop_event = threading.Event()
+        self.callback = callback
+        self.credentials_list = [dev.create_channel(transport)
+            for dev in devices]
+        self.topics = [credentials['credentials']['topic']
+            for credentials in self.credentials_list]
+        self.setDaemon(True)
+
+    def run(self):
+        """
+        Thread method, called implicitly after starting the thread.
+        """
+        creds = self.credentials_list[0]['credentials']
+        c = self.client = mqtt.Client(client_id=creds['clientId'])
+        c.on_connect = self.on_connect
+        c.on_disconnect = self.on_disconnect
+        c.on_message = self.on_message
+        c.on_subscribe = self.on_subscribe
+        c.on_unsubscribe = self.on_unsubscribe
+        c.username_pw_set(creds['user'], creds['password'])
+
+        # only encryption, no authentication
+        c.tls_insecure_set(True)
+        null_file = '/dev/null' if platform.system() != 'Windows' else 'nul'
+        c.tls_set(ca_certs=null_file, cert_reqs=ssl.CERT_NONE)
+
+        c.connect('mqtt.relayr.io', port=8883, keepalive=60)
+        try:
+            c.loop_forever()
+        except KeyboardInterrupt:
+            self.stop()
+
+        while not self._stop_event.is_set():
+            time.sleep(1)
+
+    def stop(self):
+        """
+        Mark the connection/thread for being stopped.
+        """
+        if not self._stop_event.is_set():
+            for t in self.topics:
+                # self.client.unsubscribe(t.encode('utf-8'))
+                self.client.unsubscribe(t)
+        self._stop_event.set()
+        self.client.disconnect()
+
+    def on_connect(self, client, userdata, flags, rc):
+        if not self._stop_event.is_set():
+            for t in self.topics:
+                self.client.subscribe(t.encode('utf-8'))
+
+    def on_disconnect(self, client, userdata, rc):
         pass
 
-    def subscribe(self, channel_name, callback):
-        raise NotImplementedError
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        pass
 
-    def unsubscribe(self, channel_name):
-        raise NotImplementedError
+    def on_unsubscribe(self, client, userdata, mid):
+        pass
+
+    def on_message(self, client, userdata, msg):
+        self.callback(msg.topic, msg.payload)
+
+    def add_device(self, device):
+        "Add a specific device to the MQTT connection to receive data from."
+        # create credentials
+        creds = device.create_channel('mqtt')
+        self.credentials_list.append(creds)
+        # extract topic
+        topic = creds['credentials']['topic']
+        self.topics.append(topic)
+        # subscribe topic
+        self.client.subscribe(topic.encode('utf-8'))
+
+    def remove_device(self, device):
+        "Remove a specific device from the MQTT connection to no longer receive data from."
+        # find respective credentials
+        dummy_creds = device.create_channel('mqtt')
+        creds = [c for c in self.credentials_list
+            if c['credentials']['topic'] == dummy_creds['credentials']['topic']][0]
+        # remove from self.credentials_list and self.topics
+        self.credentials_list.remove(creds)
+        topic = creds['credentials']['topic']
+        self.topics.remove(topic)
+        # unsubscribe topic
+        self.client.unsubscribe(topic.encode('utf-8'))
 
 
 if config.dataConnectionHubName == 'PubNub':
     Connection = PubnubDataConnection
 elif config.dataConnectionHubName == 'MQTT':
-    Connection = MqttDataConnection
+    Connection = MqttStream
--- tests/test_data_access.py
+++ tests/test_data_access.py
@@ -5,7 +5,15 @@ This module contains tests of the Relayr restful HTTP API client.
 
 Attention: The values in these fixture files need to be set properly before
 running the tests!
+
+Some of these tests write on stdout which gets suppressed by py.test,
+unless it is called on the command-line with the '-s' option. Some
+need to be stopped with CTRL-C, and are skipped here, but can be run
+individually when removing the skipping decorator.
 """
+
+import ssl
+import platform
 
 import pytest
 
@@ -15,7 +23,7 @@ class TestPubNub(object):
 
     def receive(self, message, channel):
         "Callback method for data connection."
-        print(message) # suppressed by py.test
+        print(message) # suppressed by py.test, unless called with -s
         self.received_data.append(message)
 
     def test_read_pubnub_device_10s(self, fix_registered):
@@ -37,3 +45,213 @@ class TestPubNub(object):
         assert len(self.received_data) > 0
         for item in self.received_data:
             assert 'ts' in item
+
+
+class TestMqttChannelCreds(object):
+    "Test creating MQTT channel credentials."
+
+    def test_repeated_creation(self, fix_registered):
+        "Test repeatedly creating channel credentials."
+        from relayr import Api
+        import paho.mqtt.client as mqtt
+        token = fix_registered.testset1['token']
+        api = Api(token=token)
+        deviceId = fix_registered.testset1['deviceID']
+
+        # test clientIds are different
+        creds_1 = api.post_channel(deviceId, 'mqtt')
+        creds_2 = api.post_channel(deviceId, 'mqtt')
+        assert creds_1['credentials']['clientId'] != creds_2['credentials']['clientId']
+
+        # test the rest is the same
+        del creds_1['credentials']['clientId']
+        del creds_2['credentials']['clientId']
+        assert creds_1 == creds_2
+
+        # cleanup, removing created stuff
+        channelId = creds_1['channelId']
+        api.delete_channel_id(channelId)
+        channels = api.get_device_channels(deviceId)['channels']
+        channelIds = [ch['channelId'] for ch in channels]
+        assert channelId not in channelIds
+
+
+class TestMqttStreamsShort(object):
+    "Test accessing device data via a single topic MQTT stream."
+
+    def test_read_mqtt_single_device_short(self, fix_registered):
+        "Test connect to a single device via MQTT and read data."
+
+        import time
+        from relayr import Client
+        from relayr.resources import Device
+        from relayr.dataconnection import MqttStream
+
+        token = fix_registered.testset1['token']
+        c = Client(token=token)
+        deviceId = fix_registered.testset1['deviceID']
+        dev = Device(id=deviceId, client=c)
+        received = []
+
+        def callback(topic, payload):
+            received.append((topic, payload))
+
+        conn = MqttStream(callback, [dev], transport='mqtt')
+        conn.start()
+        time.sleep(10)
+        conn.stop()
+        # dev.delete_channel(creds['channelId'])
+        assert len(received) >= 5
+
+
+@pytest.mark.skipif(True, reason="Needs a CTRL-C to be stopped.")
+class TestMqttStreams(object):
+    "Test accessing device data via a single topic MQTT stream."
+
+    def test_read_mqtt_single_device(self, fix_registered):
+        "Test connect to a single device via MQTT and read data."
+        import time
+        from relayr import Client
+        from relayr.resources import Device
+        import paho.mqtt.client as mqtt
+
+        token = fix_registered.testset1['token']
+        c = Client(token=token)
+        deviceId = fix_registered.testset1['deviceID']
+        d = Device(id=deviceId, client=c)
+
+        creds = c.api.post_channel(d.id, 'mqtt')
+        print('full channel creds:', creds)
+        creds = creds['credentials']
+
+        self.topics = [creds['topic'].encode('utf-8')]
+
+        client = mqtt.Client(client_id=creds['clientId'])
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.on_subscribe = self.on_subscribe
+        client.username_pw_set(creds['user'], creds['password'])
+
+        null_file = '/dev/null' if platform.system() != 'Windows' else 'nul'
+        client.tls_set(ca_certs=null_file, cert_reqs=ssl.CERT_NONE)
+        client.tls_insecure_set(True)
+
+        client.connect('mqtt.relayr.io', port=8883, keepalive=60)
+        try:
+            client.loop_forever()
+        except KeyboardInterrupt:
+            print('')
+
+    def on_connect(self, client, userdata, flags, rc):
+        for t in self.topics:
+            client.subscribe(t)
+
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        pass
+
+    def on_message(self, client, userdata, msg):
+        print('received: %s %s' % (msg.topic, msg.payload))
+
+    def receive(self, message, channel):
+        "Callback method for data connection."
+        print(message)
+        self.received_data.append(message)
+
+
+@pytest.mark.skipif(True, reason="Needs a CTRL-C to be stopped.")
+class TestMqttMultipleStreams(object):
+    "Test accessing device data via a multiple topics MQTT streams."
+
+    def test_read_mqtt_multi_device(self, fix_registered):
+        "Test connect to multiple devices via MQTT and read data."
+        import time
+        from relayr import Client, Api
+        from relayr.resources import Device
+        import paho.mqtt.client as mqtt
+
+        token = fix_registered.testset1['token']
+        c = Client(token=token)
+        deviceId = fix_registered.testset1['deviceID']
+        deviceId2 = fix_registered.testset1['deviceID2']
+
+        mic = Device(id=deviceId, client=c)
+        mic_creds = c.api.post_channel(mic.id, 'mqtt')
+        print('mic channel creds:', mic_creds)
+        mic_creds = mic_creds['credentials']
+
+        temphum = Device(id=deviceId2, client=c)
+        temphum_creds = c.api.post_channel(temphum.id, 'mqtt')
+        print('temphum channel creds:', temphum_creds)
+        temphum_creds = temphum_creds['credentials']
+
+        self.topics = [
+            mic_creds['topic'].encode('utf-8'),
+            temphum_creds['topic'].encode('utf-8')
+        ]
+
+        client = mqtt.Client(client_id=mic_creds['clientId'])
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.on_subscribe = self.on_subscribe
+        client.username_pw_set(mic_creds['user'], mic_creds['password'])
+
+        null_file = '/dev/null' if platform.system() != 'Windows' else 'nul'
+        client.tls_set(ca_certs=null_file, cert_reqs=ssl.CERT_NONE)
+        client.tls_insecure_set(True)
+
+        client.connect('mqtt.relayr.io', port=8883, keepalive=60)
+        try:
+            client.loop_forever()
+        except KeyboardInterrupt:
+            print('')
+
+    def on_connect(self, client, userdata, flags, rc):
+        for t in self.topics:
+            client.subscribe(t)
+
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        pass
+
+    def on_message(self, client, userdata, msg):
+        print('received: %s %s' % (msg.topic, msg.payload))
+
+    def receive(self, message, channel):
+        "Callback method for data connection."
+        print(message)
+        self.received_data.append(message)
+
+
+class TestDynmicMqttStreams(object):
+    "Test assigning devices to one MQTT stream and reading their data."
+
+    def receive(self, topic, payload):
+        "Callback method for receiving device data."
+        print('%s %s' % (topic, payload))
+
+    def test_read_mqtt_multi_device_abstract(self, fix_registered):
+        "Test add/remove devices dynamically and read their data."
+        import time
+        from relayr import Client
+        from relayr.resources import Device
+        from relayr.dataconnection import MqttStream
+
+        fixture = fix_registered.testset1
+
+        c = Client(token=fixture['token'])
+
+        # create channel with one device (microphone)
+        mic = Device(id=fixture['deviceID'], client=c)
+        stream = MqttStream(self.receive, [mic])
+        stream.start()
+        time.sleep(5)
+
+        # add one device (temperature/humidity)
+        temphum = Device(id=fixture['deviceID2'], client=c)
+        stream.add_device(temphum)
+        time.sleep(5)
+
+        # remove one device (microphone)
+        stream.remove_device(mic)
+        time.sleep(5)
+
+        stream.stop()
